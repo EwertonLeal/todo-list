38cd7245db3a96b43fcea0e627d40bcf06a5c68a
EwertonLeal <ewertonolivaleal6@gmail.com>
2024-03-15 11:35:45 -0300

    criação de um to-do list simples usando angular, tailwind.css e angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ITodo } from './models/todo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'todo-list';
+export class AppComponent implements OnInit {
+  formNewCardCreator!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  todo: any[] = [];
+  doing: any[] = [];
+  review: any[] = [];
+  done: any[] = [];
+
+  ngOnInit(): void {
+    this.formNewCardCreator = this.formBuilder.group({
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+    });
+
+    if (localStorage.getItem('todo')) {
+      let todo: any = localStorage.getItem('todo');
+      this.todo = JSON.parse(todo);
+    }
+
+    if (localStorage.getItem('doing')) {
+      let doing: any = localStorage.getItem('doing');
+      this.doing = JSON.parse(doing);
+    }
+
+    if (localStorage.getItem('review')) {
+      let review: any = localStorage.getItem('review');
+      this.review = JSON.parse(review);
+    }
+
+    if (localStorage.getItem('done')) {
+      let done: any = localStorage.getItem('done');
+      this.done = JSON.parse(done);
+    }
+  }
+
+  addNewCard(event: SubmitEvent) {
+    event.preventDefault();
+
+    const newCard: ITodo = {
+      id: crypto.randomUUID(),
+      title: this.formNewCardCreator.get('title')?.value,
+      description: this.formNewCardCreator.get('description')?.value,
+    };
+
+    this.todo.unshift(newCard);
+
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+
+    this.formNewCardCreator.reset();
+  }
+
+  updateLocalStorage() {
+    localStorage.setItem('todo', JSON.stringify(this.todo));
+    localStorage.setItem('doing', JSON.stringify(this.doing));
+    localStorage.setItem('review', JSON.stringify(this.review));
+    localStorage.setItem('done', JSON.stringify(this.done));
+  }
+
+  removeCard(id: string){
+    if(this.todo.find(x => x.id == id)) {
+      this.todo = this.todo.filter(x => x.id !== id);
+    }
+    
+    if(this.doing.find(x => x.id == id)) {
+      this.doing = this.doing.filter(x => x.id !== id);
+    }
+
+    if(this.review.find(x => x.id == id)) {
+      this.review = this.review.filter(x => x.id !== id);
+    }
+
+    if(this.done.find(x => x.id == id)) {
+      this.done = this.done.filter(x => x.id !== id);
+    }
+  }
+
+  drop(event: CdkDragDrop<string[] | any[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.updateLocalStorage();
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      this.updateLocalStorage();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import {MatInputModule} from '@angular/material/input';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {CdkMenuModule} from '@angular/cdk/menu';
+import {TextFieldModule} from '@angular/cdk/text-field';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    DragDropModule,
+    CdkMenuModule,
+    TextFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    provideAnimations()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
